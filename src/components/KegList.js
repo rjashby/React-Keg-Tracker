f3670e29577ad43f3482e8d79d6d44b5669cee31
Ryan Ashby <rjapsu160@gmail.com>
2022-04-22 08:48:45 -0700

    add base code to KegList.js with props

---
+++ src/components/KegList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Keg from "./Keg";
+import PropTypes from "prop-types";
+
+function KegList(props){
+
+  return (
+    <React.Fragment>
+      <hr/>
+      {props.kegList.map((keg) =>
+        <Keg
+          whenTicketClicked = { props.onKegSelection }
+          name = {keg.name}
+          brand = {keg.brand}
+          price = {keg.price}
+          alcoholContent = {keg.alcoholContent}
+          id = {keg.id}
+          key = {keg.id}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+KegList.propTypes = {
+  kegList: PropTypes.array,
+  onKegSelection: PropTypes.func
+};
+
+export default KegList;
