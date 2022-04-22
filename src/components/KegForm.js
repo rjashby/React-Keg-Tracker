2534022fef1e9bfee112e647654ce9a8d477a0dc
Ryan Ashby <rjapsu160@gmail.com>
2022-04-22 08:56:10 -0700

    add base code for KegForm.js

---
+++ src/components/KegForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function KegForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name' />
+        <input
+          type='text'
+          name='brand'
+          placeholder='Brand' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Price' />
+        <input
+          type='text'
+          name='alcoholContent'
+          placeholder='Alcohol Content' />
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+KegForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default KegForm;
